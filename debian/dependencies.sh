8e8e62e4d2d1656ed17e8a052632f09000b4241a
tschmidt <tschmidt@alioth.debian.org>
2005-10-03 11:27:51 +0000

    vdr: updated dependencies.sh to the version in Tobias's repository SVN: trunk@1376

--- debian/dependencies.sh
+++ debian/dependencies.sh
@@ -6,6 +6,6 @@ LAST_VERSION=$(echo $FIRST_VERSION | sed -e 's/-[^-]*$//')-9999
 
 # Set conflicts with previous and next vdr version in control
 for p in $(dh_listpackages); do
-    echo "vdr:Depends=  vdr (>= $FIRST_VERSION)" >> debian/$p.substvars
-    echo "vdr:Conflicts=vdr (>>  $LAST_VERSION)" >> debian/$p.substvars
+    echo "vdr:Depends=vdr (>= $FIRST_VERSION)" >> debian/$p.substvars
+    echo "vdr:Conflicts=vdr (>> $LAST_VERSION)" >> debian/$p.substvars
 done
