d4a7356212e1600ae67ae25a9b8cb07f8797581f
etobi <git@e-tobi.net>
2013-04-09 20:45:36 +0200

    Dropped the debian/rules patchinfo target
    The patchinfo is now generated with debian/rules accept-patches

--- debian/patchcheck.py
+++ debian/patchcheck.py
@@ -3,35 +3,59 @@
 import re
 import hashlib
 import os
+import subprocess
+from textwrap import TextWrapper
 from optparse import OptionParser
 
 PATCHES_FILE = 'debian/.vdr-patches'
+PATCH_INFO_FILE = 'debian/patchinfo'
 
-def get_active_patches():
-    active_patches = {}
-    for line in open("debian/patches/series", "r"):
-        match = re.match('^(?!00_)([^#]+)', line.rstrip())
+def collect_patch_info():
+    patchInfo = []
+    for patch in subprocess.check_output('quilt series', shell=True).splitlines():
+        md5 =  hashlib.md5(open('debian/patches/' + patch).read()).hexdigest()
+        header = subprocess.check_output("quilt header '%s'" % patch, shell=True)
+        author = description = None
+        match = re.search('^Author: (.*)', header, re.MULTILINE)
         if match:
-            patchFileName = "debian/patches/" + match.group(1)
-            if not os.path.exists(patchFileName):
-                patchFileName += ".patch"
-            if os.path.exists(patchFileName):
-                active_patches[patchFileName] = hashlib.md5(open(patchFileName).read()).hexdigest()
-    return active_patches
+            author = match.group(1)
+        match = re.search('^Description: ((.*?)\n( .*?\n)*)', header, re.DOTALL)
+        if match:
+            description = re.sub(r'^ \.?', '', match.group(1), 0, re.MULTILINE)
+            description = re.sub(r'^([^.].*)\n', r'\1 ', description, 0, re.MULTILINE)
+        if author and description:
+             patchInfo.append((patch, md5, author, description))
+        else:
+            print 'Incomplete patch header in %s' % patch
+            exit(1)
+    return patchInfo
 
 def get_last_patches():
-    lastPatches = {}
+    lastPatches = []
     for line in open(PATCHES_FILE, "r"):
         match = re.match('(.+):(.+)', line.rstrip())
         if match:
-            lastPatches[match.group(1)] = match.group(2)
+            lastPatches.append((match.group(1), match.group(2)))
     return lastPatches
 
-def update_patchlist():
+def generate_patchlist(patchInfo):
     patchListFile = open(PATCHES_FILE, "w")
-    patches = get_active_patches()
-    for fileName in patches:
-        patchListFile.write(fileName + ":" + patches[fileName] + "\n")
+    for (fileName, md5, author, description) in patchInfo:
+        print >>patchListFile, "%s:%s" %  (fileName, md5)
+
+def generate_patchinfo(patchInfo):
+    patchInfoFile = open(PATCH_INFO_FILE, "w")
+    print >>patchInfoFile, 'Patches applied to vanilla vdr sources'
+    print >>patchInfoFile, '--------------------------------------'
+    print >>patchInfoFile
+    for (fileName, md5, author, description) in patchInfo:
+        print >>patchInfoFile, fileName
+        print >>patchInfoFile, '    ' + author
+        print >>patchInfoFile
+        wrapper = TextWrapper(initial_indent='    ', subsequent_indent='    ', break_on_hyphens=False, width=80)
+        for paragraph in description.splitlines():
+            print >>patchInfoFile, wrapper.fill(paragraph)
+            print >>patchInfoFile
 
 def report_patches(patches, reportText):
     if len(patches) > 0:
@@ -41,15 +65,15 @@ def report_patches(patches, reportText):
         print
 
 def check_patches():
-    active_patches = get_active_patches()
+    current_patches = [(p[0], p[1]) for p in collect_patch_info()]
     last_patches = get_last_patches()
 
-    new_patches = [p for p in active_patches if last_patches.keys().count(p) == 0]
-    removed_patches = [p for p in last_patches if active_patches.keys().count(p) == 0]
-    changed_patches = [p for p in last_patches if p in active_patches and active_patches[p] != last_patches[p]]
+    new_patches = set(p[0] for p in current_patches) - set(p[0] for p in last_patches)
+    removed_patches = set(p[0] for p in last_patches) - set(p[0] for p in current_patches)
+    changed_patches = set(p[0] for p in (set(last_patches) - set(current_patches))) - set(removed_patches)
 
     report_patches(new_patches, "The following patches are new:")
-    report_patches(removed_patches, "The following patches have been disabled:") 
+    report_patches(removed_patches, "The following patches have been disabled:")
     report_patches(changed_patches, "The following patches have been modified:")
 
     if len(new_patches) + len(removed_patches) + len(changed_patches) > 0:
@@ -75,6 +99,8 @@ parser.add_option("-c", "--check", action="store_true", dest="doCheck", help="ch
 if options.doCheck:
     check_patches()
 elif options.doUpdate:
-    update_patchlist()
+    patchInfo = collect_patch_info()
+    generate_patchlist(patchInfo)
+    generate_patchinfo(patchInfo)
 else:
     parser.print_help()
